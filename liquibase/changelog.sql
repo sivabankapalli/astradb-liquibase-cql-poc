8a26de86529d22095a9fd1553d2ea24d3789cc70
siva-bankapalli <siva.bankapalli@yahoo.com>
2026-01-28 12:28:12 +1300

    validate and migrate

--- liquibase/changelog.sql
+++ liquibase/changelog.sql
@@ -1,10 +1,12 @@
--- changeset poc:1
-CREATE TABLE customer (
+--liquibase formatted sql
+
+--changeset poc:1-create-customer-table splitStatements:false context:prod
+CREATE TABLE IF NOT EXISTS customer1 (
   customer_id text PRIMARY KEY,
   full_name text,
   email text,
   created_at timestamp
-);
+) WITH default_time_to_live = 0;
 
--- changeset poc:2
-CREATE INDEX customer_email_idx ON customer (email);
+--changeset poc:2-create-customer-index splitStatements:false context:prod
+CREATE INDEX IF NOT EXISTS customer_email_idx ON customer1 (email);
